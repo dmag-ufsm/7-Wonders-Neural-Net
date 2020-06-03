9197650edb1a1a05b5d666de9419a10981b2d23c
ggpereira <ggomesper@gmail.com>
2020-06-03 17:23:52 -0300

    fix load model

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@
 # Bot to play 7 Wonders
 import torch 
 import torch.nn as nn
-
+from nn7wonders import Net 
 
 # load model
 PATH = "./models/seven_net.pth"
-model = torch.load(PATH)
+# TODO: get input_size, hidden_size, num_classes from same location wich nn7wonders 
+model = Net(21, 42, 21)
+model.load_state_dict(torch.load(PATH))
+model.eval()
 print(model)
-
 
 # bot logic
